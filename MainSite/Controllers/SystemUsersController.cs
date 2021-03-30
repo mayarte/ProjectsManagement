f850302b221c86da660eb0abaadec79adf552773
mayarte <78853184+mayarte@users.noreply.github.com>
2021-03-30 05:22:08 -0700

    اضافة كونترولر اليوزر

---
+++ MainSite/Controllers/SystemUsersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Entity;
+using Entity.MyData;
+
+namespace MainSite.Controllers
+{
+    public class SystemUsersController : Controller
+    {
+        private TheDBContext db = new TheDBContext();
+
+        // GET: SystemUsers
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+        // GET: SystemUsers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SystemUser systemUser = db.Users.Find(id);
+            if (systemUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(systemUser);
+        }
+
+        // GET: SystemUsers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SystemUsers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Email,Phone,Password")] SystemUser systemUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(systemUser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(systemUser);
+        }
+
+        // GET: SystemUsers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SystemUser systemUser = db.Users.Find(id);
+            if (systemUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(systemUser);
+        }
+
+        // POST: SystemUsers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Email,Phone,Password")] SystemUser systemUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(systemUser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(systemUser);
+        }
+
+        // GET: SystemUsers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SystemUser systemUser = db.Users.Find(id);
+            if (systemUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(systemUser);
+        }
+
+        // POST: SystemUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SystemUser systemUser = db.Users.Find(id);
+            db.Users.Remove(systemUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
